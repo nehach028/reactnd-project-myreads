aff57dc57b3b33fcaa1ddc87cf9d1191723726bf
Neha Chaturvedi <neha.chaturvedi028@gmail.com>
2018-06-21 17:27:43 +1000

    ADD:search functionality intial statge

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types';
 
 class BookSearch extends Component{
+
+	static propTypes={
+		books:PropTypes.array.isRequired,
+		onChange:PropTypes.func.isRequired
+	}
+	state={
+		query:'',
+
+	}
+	onSearchInput=(event)=>{
+		this.setState({
+			query:event.target.value,
+		})
+
+	}
+	searchBoooksByQuery=(query)=>{
+		this.props.onChange(query)
+	}
 	render(){
+
+		const{query}=this.state
+		const{books,onChange}=this.props
+		const searchedBooks=query===''
+							?	''
+							: this.searchBoooksByQuery(query)
+		console.log("result:",searchedBooks)
 		return(
 			<div className="search-books">
             	<div className="search-books-bar">
-		            <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-		            <div className="search-books-input-wrapper">
-			            {/*
-			                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-			                  You can find these search terms here:
-			                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-			                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-			                  you don't find a specific author or title. Every search is limited by search terms.
-			            */}
-		            	<input type="text" placeholder="Search by title or author"/>
+		            <Link
+		            	className="close-search"
+		            	to="/">Close</Link>
+					<div className="search-books-input-wrapper">
+			            <input type="text" placeholder="Search by title or author" onChange={this.onSearchInput} value={query}/>
 					</div>
             	</div>
 	            <div className="search-books-results">
